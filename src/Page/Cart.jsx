5d6732b88bccbfc0b1ec600a15f2032d1b70a300
Havochalo <officialmail85@gmail.com>
2023-09-11 18:41:31 +1000

    Checkout page ccreated

--- src/Page/Cart.jsx
+++ src/Page/Cart.jsx
@@ -1,3 +1,236 @@
+import { styled } from "styled-components";
+import { Navbar } from "../Components/Navbar";
+import { Announcement } from "../Components/Announcement";
+import { Footer } from "../Components/Footer";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+const Title = styled.h1`
+  text-align: center;
+  font-weight: 300;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
+
+const TopTexts = styled.div``;
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 20px;
+`;
+const Bottom = styled.div`
+  justify-content: space-between;
+  display: flex;
+`;
+
+const Info = styled.div`
+  flex: 3;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px;
+`;
+const ProductDetail = styled.div`
+  flex: 2;
+  gap: 10px;
+  display: flex;
+`;
+const Image = styled.img`
+  height: 200px;
+  width: 200px;
+`;
+const Details = styled.div`
+  /* gap: 10px; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const ProductName = styled.span``;
+const ProductId = styled.span``;
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+const ProductSize = styled.span``;
+const PriceDetail = styled.div`
+  flex: 1;
+  gap: 30px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 300;
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+  text-transform: uppercase;
+`;
+const SummaryItem = styled.div`
+  display: flex;
+  margin: 30px 0px;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+const SumaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+const Button = styled.button`
+  width: 100%;
+  color: white;
+  background-color: black;
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+  font-weight: 600;
+`;
 export const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <Container>
+      <Announcement />
+      <Navbar />
+      <Wrapper>
+        <Title>Your Bag</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+
+          <TopTexts>
+            <TopText>Shopping bag(3)</TopText>
+            <TopText>Your Whislist</TopText>
+          </TopTexts>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://images.unsplash.com/photo-1604176354204-9268737828e4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80" />
+
+                <Details>
+                  <ProductName>
+                    <b>Product Name: </b>Test Product
+                  </ProductName>
+                  <ProductId>
+                    <b>Product ID:</b> 132312
+                  </ProductId>
+                  <ProductColor color="black" />
+
+                  <ProductSize>
+                    <b>Size:</b> XL
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <AddIcon />
+
+                  <ProductAmount>$20</ProductAmount>
+                  <RemoveIcon />
+                </ProductAmountContainer>
+                <ProductPrice>$30</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://images.unsplash.com/photo-1604176354204-9268737828e4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2080&q=80" />
+
+                <Details>
+                  <ProductName>
+                    <b>Product Name: </b>Test Product
+                  </ProductName>
+                  <ProductId>
+                    <b>Product ID:</b> 132312
+                  </ProductId>
+                  <ProductColor color="black" />
+
+                  <ProductSize>
+                    <b>Size:</b> XL
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <AddIcon />
+
+                  <ProductAmount>$20</ProductAmount>
+                  <RemoveIcon />
+                </ProductAmountContainer>
+                <ProductPrice>$30</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>Order Summary</SummaryTitle>
+            <SummaryItem>
+              <SumaryItemText>SubTotal</SumaryItemText>
+              <SummaryItemPrice>$80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SumaryItemText>Estimated Shipping</SumaryItemText>
+              <SummaryItemPrice>$5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SumaryItemText>Shipping Discout</SumaryItemText>
+              <SummaryItemPrice>$-5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SumaryItemText>Total</SumaryItemText>
+              <SummaryItemPrice>$80</SummaryItemPrice>
+            </SummaryItem>
+
+            <Button>CHECK OUT</Button>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
 };
